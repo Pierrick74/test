14d4fbefbd4c000a3ccd827b58fc2f523c13425e
pierrick <pierrickviret@gmail.com>
2026-06-04 14:01:48 +0200

    add select

--- hello.go
+++ hello.go
@@ -10,7 +10,7 @@ import (
 func main() {
 
 	defer func(start time.Time) {
-        fmt.Printf("temps bubble %v\n", time.Since(start))
+        fmt.Printf("temps select fr %v\n", time.Since(start))
     }(time.Now())
 
 	datas, err := utils.ReadCSV("fr.csv");
@@ -24,7 +24,7 @@ func main() {
 	//remove de j à j+1 not include
 	datas = slices.Delete(datas, 0 , 1);
 
-	orderDatas := utils.TriInsertInt(datas, 9)
+	orderDatas := utils.TriSelectInt(datas, 9)
 
 	orderDatas = append([][]string{header}, orderDatas...)
 
---
+++ util/select.go
@@ -0,0 +1,40 @@
+package utils
+
+import "strconv"
+
+func TriSelectInt(tab [][]string, col int) [][]string {
+	//converti en nombre
+  	keys := make([]int64, len(tab));
+  		for i := range tab {
+    	keys[i], _ = strconv.ParseInt(tab[i][col], 10,  64);
+  	}
+  
+	index := 0;
+    var val int64 = 0;
+
+	for i:=0; i<len(tab); i++{
+    	index = i;
+    	val = 0;
+
+		for j:=i; j<len(tab); j++ {
+       		if((val > keys[j]) || val == 0){
+         		index = j;
+         		val = keys[j];
+       		}
+    	}
+
+		temp := tab[index];
+    	tempKey := keys[index];
+
+    	for k := index; k > i; k-- {
+       		tab[k] = tab[k-1];
+       		keys[k] = keys[k-1];
+    	}
+    	tab[i] = temp;
+    	keys[i] = tempKey;
+  	}
+
+
+  return tab;
+}
+
